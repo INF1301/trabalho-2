3655d05d1aadf8f901db3f07e186215c2c5af42b
hydratedguy <andre.ryc1@gmail.com>
2021-03-23 18:30:54 -0300

    v1

--- pilha.c
+++ pilha.c
@@ -10,22 +10,27 @@ struct pilha {
 Pilha *pilha_cria(void) {
 	Pilha* p;
 	p = (Pilha*)malloc(sizeof(Pilha));
-	p = lst_cria();
+	if (p == NULL) {
+		printf("Problema de memoria!");
+		exit(0);
+	}
+	p->lst = lst_cria();
 	return p;
 }
 
 int pilha_vazia(Pilha *p) {
-	return lst_vazia(p);
+	return lst_vazia(p->lst);
 }
 
 void pilha_push(Pilha* p, void *v) {
-	lst_insIni(p, v);
+	lst_insIni(p->lst, v);
 }
 
 void *pilha_pop(Pilha* p) {
-	return lst_retIni(p);
+	return lst_retIni(p->lst);
 }
 
 void pilha_libera(Pilha *p) {
-	lst_libera(p);
+	lst_libera(p->lst);
+	free(p);
 }
